8a583f0d03f60072fe1b3877028d78cd56023d3f
oo009pbh <oo009pbh@gmail.com>
2022-07-13 23:27:19 +0900

    [7월 13일] 숨바꼭질 2.py

--- 7월 11일 ~/숨바꼭질 2.py
+++ 7월 11일 ~/숨바꼭질 2.py
@@ -2,27 +2,33 @@ from sys import stdin
 from collections import deque
 
 def BFS (N, K):
+    visited = [False] * 100001
     queue = deque([(N, 0)])
-    min_value = -1
-    min_value_count = 0
+    min_count = 1e9
+    min_counts = 0
 
     while queue:
         n, count = queue.popleft()
+        visited[n] = True
 
-        if min_value != -1 and min_value < count:
-            return min_value, min_value_count
+        if n == K :
+            if count == min_count:
+                min_counts += 1
+            elif count < min_count:
+                min_count = count
+                min_counts = 1
+                
+        if count + 1 > min_count:
+            continue
 
-        if n == K:
-            min_value = count
-            min_value_count += 1
-
-        if n < K: 
+        if n * 2 <= 100000 and not visited[n * 2]:
             queue.append((n * 2, count + 1))
+        if n + 1 <= 100000 and not visited[n + 1]:            
             queue.append((n + 1, count + 1))
-        if n - 1 > 0:
+        if n - 1 >= 0 and not visited[n - 1]:
             queue.append((n - 1, count + 1))
 
-    return min_value, min_value_count
+    return min_count, min_counts
 
 N, K = map(int, stdin.readline().rstrip().split())
 min_value, min_value_count = BFS(N, K)
