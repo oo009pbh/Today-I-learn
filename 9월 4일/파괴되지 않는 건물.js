d62ae1d511b51330579f573af83186f15b8024c7
oo009pbh <oo009pbh@gmail.com>
2022-09-08 00:45:50 +0900

    [9월 7일] 파괴되지 않는 건물

--- 9월 4일/파괴되지 않는 건물.js
+++ 9월 4일/파괴되지 않는 건물.js
@@ -4,23 +4,39 @@ function solution(board, skill) {
     let M = board[0].length;
     let tempBoard = Array.from(Array(N), () => new Array(M).fill(0));
 
-    for (let i = 0; i < skill.length ; i ++) {
-        for (let j = skill[i][1] ; j <= skill[i][3] ; j++) {
-            tempBoard[j][skill[i][2]] += skill[i][0] === 1 ? -skill[i][5] : skill[i][5];
-            if (skill[i][4] + 1 < M) {
-                tempBoard[j][skill[i][4] + 1] -= skill[i][0] === 1 ? -skill[i][5] : skill[i][5];
-            }
+    for (let item of skill) {
+        let [type, r1, c1, r2, c2, degree] = item;
+        degree = type === 1 ? -degree : degree;
+        tempBoard[r1][c1] += degree;
+        if (c2 + 1 < M) {
+            tempBoard[r1][c2 + 1] -= degree;
+        }
+        if (r2 + 1 < N) {
+            tempBoard[r2 + 1][c1] -= degree;
+        }
+        if (c2 + 1 < M && r2 + 1 < N) {
+            tempBoard[r2 + 1][c2 + 1] += degree;
         }
     }
     for (let i = 0 ; i < N ; i++) {
+        for (let j = 1; j < M; j++) {
+            tempBoard[i][j] += tempBoard[i][j - 1];
+        }
+    }
+    for (let i = 1 ; i < N ; i++) {
         for (let j = 0; j < M; j++) {
-            tempBoard[i][j] += j === 0 ? 0 : tempBoard[i][j - 1];
-            answer += board[i][j] + tempBoard[i][j] > 0 ? 1 : 0;
+            tempBoard[i][j] += tempBoard[i - 1][j];
+        }
+    }
+
+    for (let i = 0 ; i < N ; i++) {
+        for (let j = 0; j < M; j++) {
+            board[i][j] += tempBoard[i][j];
+            answer += board[i][j] > 0 ? 1 : 0;
         }
     }
     return answer;
 }
-
 console.log(
     solution([
             [5, 5, 5, 5, 5],
