258cb55e58c0d0928c95962727ecfb08009253c7
oo009pbh <oo009pbh@gmail.com>
2022-09-17 17:13:22 +0900

    [9월 17일] 양과늑대

--- 9월 13일/양과늑대.js
+++ 9월 13일/양과늑대.js
@@ -1,6 +1,34 @@
-function solution(board, r, c) {
+function solution(info, edges) {
     let answer = 0;
+    let graph = {};
+    for (let edge of edges) {
+        if (graph.hasOwnProperty(edge[0])) {
+            graph[edge[0]].push(edge[1]);
+        } else {
+            graph[edge[0]] = [edge[1]];
+        }
+    }
+
+    const DFS = (current, nextNodes) => {
+        let [sheep, wolf, currentNode] = current;
+        let newNextNodes = [...nextNodes];
+        let currentIndex = newNextNodes.indexOf(currentNode);
+        info[currentNode] === 0 ? sheep += 1 : wolf += 1;
+        answer = Math.max(sheep, answer);
+        if (sheep === wolf) return ;
+
+        if (graph.hasOwnProperty(currentNode)) {
+            newNextNodes.push(...graph[currentNode]);
+        }
+
+        newNextNodes.splice(currentIndex, 1);
+
+        for (let nextNode of newNextNodes) {
+            DFS([sheep, wolf, nextNode], newNextNodes);
+        }
+    };
+    DFS([0, 0, 0], [0]);
     return answer;
 }
 
-console.log(solution([[1, 0, 0, 3], [2, 0, 0, 0], [0, 0, 0, 2], [3, 0, 1, 0]], 1, 0))
+console.log(solution([0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1], [[0, 1], [1, 2], [1, 4], [0, 8], [8, 7], [9, 10], [9, 11], [4, 3], [6, 5], [4, 6], [8, 9]]))
